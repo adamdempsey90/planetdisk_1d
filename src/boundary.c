c9fe4ec8724d223b9bf9c8940d195061c3604110
Adam Dempsey <adamdemps@gmail.com>
2016-04-07 16:54:58 -0500

    Boundary conditions work.

--- src/boundary.c
+++ src/boundary.c
@@ -7,20 +7,20 @@ void set_boundary(void) {
     double bb = 3*nu(rb);
     double ab = bb*(params.gamma - .5)/rb;
 
-    matrix.md[0] += ab*(r0*nu(r0))/(rb*nu(rb)) + (3/rb)*(rb*nu(rb)-r0*nu(r0))/(r0-rb);
+    matrix.md[0] -= 1.5*nu(rb)/rb;
    // matrix.ud[0] = 0;
    // matrix.ld[NR-2] = 0;
    // matrix.md[NR-1] = 0;
     matrix.fm[NR-1] = params.bc_mdot;
     matrix.u[0] = 0;
     matrix.u[NR] = 0;
-/*
-   matrix.md[0] = 0;
-   matrix.ud[0] = 0;
-   matrix.md[NR-1] = 0;
-   matrix.ld[NR-2] = 0;
-   matrix.fm[NR-1] = params.bc_mdot;
-  */ 
+
+  // matrix.md[0] = 0;
+  // matrix.ud[0] = 0;
+  // matrix.md[NR-1] = 0;
+  // matrix.ld[NR-2] = 0;
+   //matrix.fm[NR-1] = params.bc_mdot;
+   
     return;
 
 }
--- src/mdot.c
+++ src/mdot.c
@@ -1,5 +1,27 @@
 #include "pdisk.h"
 
+
+void set_mdot(int planet_torque) {
+    int i;
+    double lamm;
+#ifndef NONLOCAL
+   for(i=1;i<NR;i++) { 
+        
+        mdot[i] = 1.5*( lam[i]*nu(rc[i])/sqrt(rc[i]) - lam[i-1]*nu(rc[i-1])/sqrt(rc[i-1]))/(sqrt(rc[i])-sqrt(rc[i-1]));
+
+        if (planet_torque) {
+            lamm = lam[i-1]*dr[i]/(dr[i-1]+dr[i]) + lam[i]*dr[i-1]/(dr[i-1]+dr[i]);
+            mdot[i] -= 2*sqrt(rmin[i])*dTr_ex(rmin[i],planet.a)*lamm;
+        }
+        
+
+    }
+    mdot[0] = lam[0]*1.5*nu(rmin[0])/rmin[0];
+#endif
+    return;
+}
+
+/*
 void set_mdot(int planet_torque) {
     int i;
     double ca, cb;
@@ -43,4 +65,4 @@ void set_mdot(int planet_torque) {
 
     return;
 }
-
+*/
